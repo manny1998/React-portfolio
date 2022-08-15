dc0a109f50223e4c309d566aff13611dddbadf9d
Manny <manroven-singh@hotmail.com>
2022-08-15 22:13:47 +0930

    adding code to about.js

---
+++ react-portfolio/src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <p className="content is-medium">Westpac Credit Assessor and studying to be full stack developer</p>
+      <hr />
+      <p className="content">
+      My name is Manroven Singh and I have worked many jobs ranging from on the farm to in a bank. I'm always looking to learn new things which is why I have undertaken the coding bootcamp.
+      </p>
+    </div>
+  );
+}
+
+export default About;
